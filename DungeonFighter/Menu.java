a7a1cf3c25228312fe2e39b78356e9b0696215a8
cadufcg <carloseduardonfcg@gmail.com>
2024-08-26 18:24:30 -0300

    Update Menu.java

--- DungeonFighter/Menu.java
+++ DungeonFighter/Menu.java
@@ -5,44 +5,32 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Menu extends JFrame implements ActionListener{
-    JButton botao1,botao2,botao3;Selecao sel;
+    JButton botao1,botao2,botao3;Selecao sel;JButton[] botoes;String[] textos;Container menu;
     Menu(){
-        Container menu= getContentPane();
+        menu= getContentPane();
         setLayout(null);
         ImageIcon image=new ImageIcon("menu.jpg");
         Image img = image.getImage(); 
-        Image resizedImage = img.getScaledInstance(960, 768, Image.SCALE_SMOOTH); 
+        Image resizedImage = img.getScaledInstance(960, 768, Image.SCALE_SMOOTH); //funcao pra enquadrar a imagem na resolucao
         JLabel background = new JLabel(new ImageIcon(resizedImage));
         background.setBounds(0,0,960,768);
         menu.add(background);
-        botao1 = new JButton("Jogar");
-        botao2 = new JButton("Debug");
-        botao3 = new JButton("Sair");
-        botao1.setBounds(700, 300, 200, 100);
-        botao1.setForeground(Color.blue);
-        botao2.setBounds(700, 400, 200, 100);
-        botao2.setForeground(Color.RED);
-        botao3.setBounds(700, 500, 200, 100);
-        botao3.setForeground(Color.DARK_GRAY);
-        botao1.addActionListener(this);
-        botao2.addActionListener(this);
-        botao3.addActionListener(this);
-        menu.add(botao1);
-        menu.add(botao2);
-        menu.add(botao3);
+        textos = new String[]{"Jogar","Debug","Sair"};
+        botoes = new JButton[3];
+        fazerbotoes(botoes, textos);
         setSize(960,768);
-        menu.setComponentZOrder(background, menu.getComponentCount() - 1);
+        menu.setComponentZOrder(background, menu.getComponentCount() - 1); //metodo para os botoes ficarem acima do background
         setVisible(true);
         setLocationRelativeTo(null); 
         getContentPane().setBackground(new Color(135,50,250));
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == botao1){
+        if(e.getSource() == botoes[0]){
             new Selecao();
-        }else if(e.getSource() == botao2){
+        }else if(e.getSource() == botoes[1]){
             new Jogador(0,0,0,0);
-        }else if(e.getSource() == botao3){
+        }else if(e.getSource() == botoes[2]){
             JFrame frame = new JFrame();
             String[] options = new String[2];
             options[0] = "Sim";
@@ -52,6 +40,15 @@ public class Menu extends JFrame implements ActionListener{
                 System.exit(0);
             }
         
+        }
+    }
+    public void fazerbotoes(JButton[] botoes,String[] textos){ //funcao para criar os botoes
+        for(int i=0;i<3;i++){
+        botoes[i] = new JButton(textos[i]);
+        botoes[i].setBounds(700, 300+i*100, 200, 100);
+        botoes[i].setForeground(Color.BLACK);
+        botoes[i].addActionListener(this);
+        menu.add(botoes[i]);
         }
     }
 }
