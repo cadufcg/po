f1369a0648a98275bc0af57e294324863c9c9fcf
cadufcg <carloseduardonfcg@gmail.com>
2024-08-26 18:34:33 -0300

    Update Inimigo.java

--- DungeonFighter/Inimigo.java
+++ DungeonFighter/Inimigo.java
@@ -4,7 +4,7 @@ public class Inimigo {
     private float vida;
     private float ataque;
     private float defesa;
-    public Inimigo(float vida,float ataque,float defesa){
+    public Inimigo(float vida,float ataque,float defesa){ //inimigo é qualquer coisa que tem vida,ataque e defesa
         this.vida=vida;
         this.ataque=ataque;
         this.defesa=defesa;
@@ -27,7 +27,7 @@ public class Inimigo {
 	public void setDefesa(float defesa) {
 		this.defesa = defesa;
 	}
-    public void ataca(Inimigo inimigo){
+    public void ataca(Inimigo inimigo){ //metodo de ataque, dano= atq*(100-defesa do inimigo)/100
         inimigo.setVida(inimigo.getVida()-(this.ataque)*(100-(inimigo.getDefesa()))/100);
     }
 }
